3464c209bce0c64a80ca7cb35a218ea04b1b35da
Krrish Raj <113225511+Krrish-Raj-16@users.noreply.github.com>
2025-07-07 23:56:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,19 +3,30 @@ public:
     int longestSubarray(vector<int>& a) 
     {
         int n=a.size();
-        int zero=0;
-        int l=0;
-        int ans=0;
-        for(int r=0;r<n;r++)
+        vector<vector<int>> dp(n+1, vector<int>(2,0));
+        dp[0][0]=a[0];
+        dp[0][1]=!a[0];
+        int tot=a[0];
+        for(int i=1;i<n;i++)
         {
-            cout<<l<<"-"<<r<<endl;
-            if(a[r]==0)zero++;
-            while(l<=r && zero>1)
+            if(a[i]==0)
             {
-                if(a[l]==0)zero--;
-                l++;
+                dp[i][0]=0;
+                dp[i][1]=dp[i-1][0];
             }
-            ans=max(ans, r-l);
+            else
+            {
+                tot++;
+                dp[i][0]=dp[i-1][0]+1;
+                dp[i][1]=dp[i-1][1]+1;
+            }
+        }
+        if(tot==n)return n-1;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            cout<<dp[i][0]<<" "<<dp[i][1]<<endl;
+            ans=max({ans,dp[i][0], dp[i][1]});
         }
         return ans;
         
