a376327b48eca0de399f0d9482b2af227a6d0358
Krrish Raj <113225511+Krrish-Raj-16@users.noreply.github.com>
2025-07-26 23:24:59 +0530

    Time: 4 ms (19.85%), Space: 11.9 MB (27.02%) - LeetHub

--- 0809-expressive-words/0809-expressive-words.cpp
+++ 0809-expressive-words/0809-expressive-words.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    vector<pair<char,int>> getV(string &s) {
+    vector<pair<char,int>> getV(string &s) 
+    {
         vector<pair<char,int>> v;
         int cur = 1;
-        for(int i = 0; i < s.length()-1; i++) {
+        for(int i = 0; i < s.length()-1; i++) 
+        {
             if(s[i] == s[i+1]) cur++;
-            else {
+            else 
+            {
                 v.push_back({s[i], cur});
                 cur = 1;
             } 
@@ -14,9 +17,11 @@ public:
         return v;
     }
 
-    bool check(vector<pair<char,int>> &v, vector<pair<char,int>> &word) {
+    bool check(vector<pair<char,int>> &v, vector<pair<char,int>> &word) 
+    {
         if (v.size() != word.size()) return false;
-        for (int i = 0; i < v.size(); ++i) {
+        for (int i = 0; i < v.size(); ++i) 
+        {
             if (v[i].first != word[i].first) return false;
             int a = v[i].second, b = word[i].second;
             if (a == b) continue;
@@ -28,7 +33,8 @@ public:
     int expressiveWords(string s, vector<string>& w) {
         int ans = 0;
         vector<pair<char,int>> v = getV(s);
-        for (auto& str : w) {
+        for (auto& str : w) 
+        {
             vector<pair<char,int>> word = getV(str);
             if (check(v, word)) ans++;
         }
