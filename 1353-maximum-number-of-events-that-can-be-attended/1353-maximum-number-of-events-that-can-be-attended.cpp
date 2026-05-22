fb9279836ef929e2b4ff418bf7d202f233c318b4
Krrish Raj <113225511+Krrish-Raj-16@users.noreply.github.com>
2026-05-22 22:40:53 +0530

    Time: 63 ms (47.84%), Space: 75.1 MB (22.65%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-	static const bool comp(const vector<int> &a,const  vector<int> &b) {
-		if (a[0] != b[0]) 	return a[0] < b[0];
-		return a[1] < b[1];
-	}
-	int maxEvents(vector<vector<int>>& events) 
-    {
-        int n = events.size();
-		int ans = 0;
-		int i = 0;
-		sort(events.begin(), events.end(), comp);
-		multiset<int> s;
-		for (int d = 1; d < 100001; ++d) 
+    int maxEvents(vector<vector<int>>& a) {
+        int d=0, n=a.size(), i=0, ans=0;
+        sort(a.begin(), a.end());
+        priority_queue<int, vector<int>, greater<>> pq;
+        while(i<n || !pq.empty())
         {
-			while (!s.empty() && *(s.begin()) < d) 
+            if(pq.empty())
             {
-				s.erase(s.begin());
-			}
-			while (i < n && events[i][0] == d) 
+                d=a[i][0];
+            }
+            while(i<n && a[i][0]==d)
             {
-				s.insert(events[i][1]);
-				i++;
-			}
-			if (s.size() > 0) 
+                pq.push(a[i][1]);
+                i++;
+            }
+            while(!pq.empty() && pq.top()<d)
             {
-				ans++;
-				s.erase(s.begin());
-			}
-		}
-		return ans;
-	}
+                pq.pop();
+            }
+            if(!pq.empty())
+            {
+                d++;
+                ans++;
+                pq.pop();
+            }
+        }
+        return ans;
+    }
 };
